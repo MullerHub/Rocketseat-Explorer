8784d7b0f088a9ec7a6af148daf67fbb21c4a6dc
murilo muller <murilomuller@protonmail.com>
2022-09-26 14:12:11 -0300

    clear init

---
+++ Stage0901/RocketNotes/src/pages/styles/global.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+  }
+
+  a {
+    text-decoration: none;
+  }
+  
+  button, a {
+    cursor: pointer;
+  }
+`
