bfdd1bb6f490d8dec226e0e06f760c58359a8cea
murilo muller <murilomuller@protonmail.com>
2022-09-26 14:57:07 -0300

    clear init

--- Stage0901/RocketNotes/src/pages/Details/index.jsx
+++ Stage0901/RocketNotes/src/pages/Details/index.jsx
@@ -1,11 +1,11 @@
 import {Container} from './styles'
+import { Button } from '../../components/Button'
 export function Details() {
 
   return(
-    <>
     <Container >
+      <Button />
      <h1>Hello guy's</h1>
     </Container>
-    </>
   )
 }
