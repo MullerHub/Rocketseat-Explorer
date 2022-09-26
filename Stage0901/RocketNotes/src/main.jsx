2ab2fead72fdec27c0f4457568fb13c19aa3e854
murilo muller <murilomuller@protonmail.com>
2022-09-26 15:54:31 -0300

    clear init

--- Stage0901/RocketNotes/src/main.jsx
+++ Stage0901/RocketNotes/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import {Details} from './pages/Details'
 import { ThemeProvider} from 'styled-components'
 import  theme from './styles/theme'
+import GLobalStyles from './styles/global'
 import './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GLobalStyles />
     <Details />
     </ThemeProvider>
   </React.StrictMode>
