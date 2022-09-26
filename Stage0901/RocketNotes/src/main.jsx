30d37f1c8522be7507ebcb12d297c859fd52dd43
murilo muller <murilomuller@protonmail.com>
2022-09-26 14:05:50 -0300

    clear init

--- Stage0901/RocketNotes/src/main.jsx
+++ Stage0901/RocketNotes/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Details} from './pages/Details'
+import { ThemeProvider} from 'styled-components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <Details />
+    </ThemeProvider>
   </React.StrictMode>
 )
