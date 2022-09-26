ecc013060af7f97afc645ed1bf916ce0b7e0d0b0
murilo muller <murilomuller@protonmail.com>
2022-09-26 16:15:39 -0300

    section add

--- Stage0901/RocketNotes/src/components/Section/styles.js
+++ Stage0901/RocketNotes/src/components/Section/styles.js
@@ -1,3 +1,11 @@
 import styled from 'styled-components'
 
-export const Container = styled.section``
+export const Container = styled.section`
+  margin: 56px 0 28px;
+
+  > h2 {
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+  }
+`
