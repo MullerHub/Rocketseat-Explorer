040e4c233d1f2b1710156b4550426b03a74e07f6
murilo muller <murilomuller@protonmail.com>
2022-09-26 16:03:40 -0300

    clear init

--- Stage0901/RocketNotes/src/components/Header/index.jsx
+++ Stage0901/RocketNotes/src/components/Header/index.jsx
@@ -1,10 +1,18 @@
-import {Container } from './styles'
+import {Container, Profile } from './styles'
 
 export function Header() {
 
   return(
     <Container>
-      
+      <Profile>
+      <img src='https://github.com/mullerhub.png'
+      alt='foto do usuario'
+       />
+       <div>
+        <span>Bem vindo</span>
+        <strong>Murilo Muller</strong>
+       </div>
+      </Profile>
     </Container>
   )
 }
