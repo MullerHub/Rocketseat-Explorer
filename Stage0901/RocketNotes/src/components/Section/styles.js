259a5f6be78881cfe5632621216d3ddd0fb99ccf
murilo muller <murilomuller@protonmail.com>
2022-09-26 16:16:49 -0300

    section add

--- Stage0901/RocketNotes/src/components/Section/styles.js
+++ Stage0901/RocketNotes/src/components/Section/styles.js
@@ -7,5 +7,12 @@ export const Container = styled.section`
     border-bottom-width: 1px;
     border-bottom-style: solid;
     border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+
+    padding-bottom: 16px;
+    margin-bottom: 24px;
+
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+    font-size: 20px;
+    font-weight: 400;
   }
 `
