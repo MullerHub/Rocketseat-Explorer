5843525a0c45171a3fa4942a5ff32918046995bc
murilo muller <murilomuller@protonmail.com>
2022-09-26 16:34:54 -0300

    Tag components add

--- Stage0901/RocketNotes/src/pages/Details/index.jsx
+++ Stage0901/RocketNotes/src/pages/Details/index.jsx
@@ -2,8 +2,7 @@ import {Container, Links} from './styles'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
-
-
+import { Tag } from '../../components/Tag'
 
 export function Details() {
 
@@ -18,7 +17,8 @@ export function Details() {
         </Links>
       </Section>
       <Section title="Marcadores" >
-      
+      <Tag title="express" />
+      <Tag title="node" />
       </Section>
 
       <Button title="Voltar" />
