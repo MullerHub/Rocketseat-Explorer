771fd658c3f54b70eaa28d9188874f239eb5ac46
murilo muller <murilomuller@protonmail.com>
2022-09-26 16:19:21 -0300

    section add

--- Stage0901/RocketNotes/src/pages/Details/index.jsx
+++ Stage0901/RocketNotes/src/pages/Details/index.jsx
@@ -1,6 +1,7 @@
 import {Container} from './styles'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
 
 
 
@@ -9,6 +10,13 @@ export function Details() {
   return(
     <Container >
       <Header/>
+      <Section title="Links úteis" >
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </Section>
       <Button title="Voltar" />
     </Container >
   )
