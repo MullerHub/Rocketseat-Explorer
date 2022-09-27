2cc409d811500d99b8958ff7d9fd68dc710aecbb
murilo muller <murilomuller@protonmail.com>
2022-09-27 15:54:34 -0300

    SignIn att

--- Stage0901/RocketNotes/src/pages/SignIn/Index.jsx
+++ Stage0901/RocketNotes/src/pages/SignIn/Index.jsx
@@ -1,5 +1,13 @@
 import { Container } from './styles'
 
 export function SignIn() {
-  return <Container>Ana kim</Container>
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p></p>
+        <h2></h2>
+      </Form>
+    </Container>
+  )
 }
