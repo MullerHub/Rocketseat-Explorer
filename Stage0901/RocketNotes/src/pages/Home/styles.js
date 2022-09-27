9bbd8ba59cd205b55e348c7aaac06af1bca34bbd
murilo muller <murilomuller@protonmail.com>
2022-09-26 22:57:49 -0300

    ButtonText add

--- Stage0901/RocketNotes/src/pages/Home/styles.js
+++ Stage0901/RocketNotes/src/pages/Home/styles.js
@@ -1,1 +1,25 @@
 import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+`
+
+export const Brand = styled.div`
+  width: 100%;
+`
+
+export const Menu = styled.ul`
+  width: 100%;
+`
+
+export const Search = styled.ul`
+  width: 100%;
+`
+
+export const Content = styled.div`
+  width: 100%;
+`
+
+export const NewNote = styled.button`
+  width: 100%;
+`
