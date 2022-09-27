28cefc14c4827bd947fa53d0b04ade7f6cd30726
murilo muller <murilomuller@protonmail.com>
2022-09-27 14:46:22 -0300

    Notes init

--- Stage0901/RocketNotes/src/components/Note/styles.js
+++ Stage0901/RocketNotes/src/components/Note/styles.js
@@ -3,4 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.button`
   width: 100%;
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+
+  border: none;
+  border-radius: 10px;
 `
