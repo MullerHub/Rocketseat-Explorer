a08c06195e3eb5cc2b891304cc05766ac4f80ad9
murilo muller <murilomuller@protonmail.com>
2022-09-27 20:15:52 -0300

    SignUp att

--- Stage0901/RocketNotes/src/main.jsx
+++ Stage0901/RocketNotes/src/main.jsx
@@ -5,13 +5,13 @@ import theme from './styles/theme'
 import React from 'react'
 import './styles/global'
 
-import { SignIn } from './pages/SignIn/Index'
+import { SignUp } from './pages/SignUp/Index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GLobalStyles />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 )
