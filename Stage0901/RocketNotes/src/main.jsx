d7e2525321ba2d2c6a7671e14bb91506fd71dc92
murilo muller <murilomuller@protonmail.com>
2022-09-27 15:52:17 -0300

    SignIn att

--- Stage0901/RocketNotes/src/main.jsx
+++ Stage0901/RocketNotes/src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
-import theme from './styles/theme'
 import GLobalStyles from './styles/global'
+import ReactDOM from 'react-dom/client'
+import theme from './styles/theme'
+import React from 'react'
 import './styles/global'
 
-import { Home } from './pages/Home'
+import { SignIn } from './pages/SignIn/Index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GLobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 )
