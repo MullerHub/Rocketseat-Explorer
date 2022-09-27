69f057b86b954565c3cc5fab387a8f06a5b088f0
murilo muller <murilomuller@protonmail.com>
2022-09-26 22:59:56 -0300

    ButtonText add

--- Stage0901/RocketNotes/src/pages/Home/styles.js
+++ Stage0901/RocketNotes/src/pages/Home/styles.js
@@ -2,6 +2,17 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 250px auto;
+  grid-template-rows: 105px 128px;
+  grid-template-areas:
+    'brand header'
+    'menu search'
+    'menu content'
+    'newnote contet';
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
 `
 
 export const Brand = styled.div`
