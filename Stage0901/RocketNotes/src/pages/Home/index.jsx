8a84c3e81b592cb8a3584ba65e04175cd89764a9
murilo muller <murilomuller@protonmail.com>
2022-09-26 22:50:40 -0300

    ButtonText add

---
+++ Stage0901/RocketNotes/src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+
+export function Home() {
+  return (
+    <Container>
+      <Brand>
+        <h1>RocketNotes</h1>
+      </Brand>
+    </Container>
+  )
+}
