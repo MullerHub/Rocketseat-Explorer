9f222e971cacde4e7dd717ea74b07ed433b440ac
murilo muller <murilomuller@protonmail.com>
2022-09-27 15:55:08 -0300

    SignIn att

--- Stage0901/RocketNotes/src/pages/SignIn/Index.jsx
+++ Stage0901/RocketNotes/src/pages/SignIn/Index.jsx
@@ -6,7 +6,7 @@ export function SignIn() {
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis</p>
-        <h2></h2>
+        <h2>Faça seu login</h2>
       </Form>
     </Container>
   )
