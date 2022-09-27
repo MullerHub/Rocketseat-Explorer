231c8df3f5e8f97cefb98ae387dc335ed5a603b9
murilo muller <murilomuller@protonmail.com>
2022-09-27 12:17:41 -0300

    Notes init

--- Stage0901/RocketNotes/src/components/Note/index.jsx
+++ Stage0901/RocketNotes/src/components/Note/index.jsx
@@ -1,7 +1,16 @@
-import {Container} from './styles'
+import { Container } from './styles'
 
-export function Note() {
-  return(
-    
+export function Note({ data, ...rest }) {
+  return (
+    <Container {...rest}>
+      <h1>{data.title}</h1>
+      {data.tags && (
+        <footer>
+          {data.tags.map(tag => {
+            ;<span key={tag.id}>{tag.name}</span>
+          })}
+        </footer>
+      )}
+    </Container>
   )
 }
