c2bf8b6dc7d69fcd92248b00ff10b8425bb711e7
murilo muller <murilomuller@protonmail.com>
2022-09-27 20:28:54 -0300

    SignUp att

--- Stage0901/RocketNotes/src/pages/SignUp/Index.jsx
+++ Stage0901/RocketNotes/src/pages/SignUp/Index.jsx
@@ -1,6 +1,6 @@
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
-import { FiMail, FiLock } from 'react-icons/fi'
+import { FiMail, FiLock, FiUser } from 'react-icons/fi'
 
 import { Background, Container, Form } from './styles'
 
@@ -13,6 +13,7 @@ export function SignUp() {
         <p>Aplicação para salvar e gerenciar seus links úteis</p>
         <h2>Crie sua conta</h2>
 
+        <Input placeholder="Nome" type="text" icon={FiUser} />
         <Input placeholder="E-mail" type="text" icon={FiMail} />
         <Input placeholder="Senha" type="password" icon={FiLock} />
         <Button title="Entrar" />
