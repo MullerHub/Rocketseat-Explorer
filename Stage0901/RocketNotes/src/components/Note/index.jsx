7eb7b8b6944c9023f299e675075a24309903cbdb
murilo muller <murilomuller@protonmail.com>
2022-09-27 14:50:20 -0300

    Notes init

--- Stage0901/RocketNotes/src/components/Note/index.jsx
+++ Stage0901/RocketNotes/src/components/Note/index.jsx
@@ -7,9 +7,9 @@ export function Note({ data, ...rest }) {
       <h1>{data.title}</h1>
       {data.tags && (
         <footer>
-          {data.tags.map(tag => {
-            ;<Tag key={tag.name} title={tag.name} />
-          })}
+          {data.tags.map(tag => (
+            <Tag key={tag.name} title={tag.name} />
+          ))}
         </footer>
       )}
     </Container>
