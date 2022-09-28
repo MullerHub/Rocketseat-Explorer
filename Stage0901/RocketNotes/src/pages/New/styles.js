bb5f9a9c134bb71d72beed42556788d67cac381f
murilo muller <murilomuller@protonmail.com>
2022-09-27 22:38:15 -0300

    New att

--- Stage0901/RocketNotes/src/pages/New/styles.js
+++ Stage0901/RocketNotes/src/pages/New/styles.js
@@ -18,7 +18,9 @@ export const Form = styled.form`
     display: flex;
     align-items: center;
     justify-content: space-between;
+
     margin-bottom: 36px;
+
     a {
       font-size: 20px;
       color: ${({ theme }) => theme.COLORS.GRAY_100};
