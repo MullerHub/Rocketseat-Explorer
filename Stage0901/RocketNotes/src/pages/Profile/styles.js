6c37555963cf749401eb4d63d279f025b039185b
murilo muller <murilomuller@protonmail.com>
2022-09-27 22:17:11 -0300

    Profile att

--- Stage0901/RocketNotes/src/pages/Profile/styles.js
+++ Stage0901/RocketNotes/src/pages/Profile/styles.js
@@ -22,11 +22,15 @@ export const Container = styled.div`
 export const Form = styled.form`
   max-width: 340px;
   margin: 30px auto 0;
+
+  > div:nth-child(4) {
+    margin-top: 29px;
+  }
 `
 
 export const Avatar = styled.div`
   position: relative;
-  margin: 0 auto 32px;
+  margin: -124px auto 32px;
   width: 186px;
   height: 186px;
 
