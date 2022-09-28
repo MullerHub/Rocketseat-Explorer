a1ea3e1a2ec287cd92c472afba832ce11cba18a5
murilo muller <murilomuller@protonmail.com>
2022-09-27 23:25:46 -0300

    NoteItems att

--- Stage0901/RocketNotes/src/components/NoteItem/styles.js
+++ Stage0901/RocketNotes/src/components/NoteItem/styles.js
@@ -24,5 +24,16 @@ export const Container = styled.div`
   > input {
     height: 56px;
     width: 100%;
+
+    padding: 12px;
+
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    background: transparent;
+
+    border: none;
+
+    &::placeholder {
+      color: ${({ theme }) => theme.COLORS.GRAY_300};
+    }
   }
 `
