63bf277dc26be03c6133b2a3bf869f598269d5e0
murilo muller <murilomuller@protonmail.com>
2022-09-27 21:18:18 -0300

    Profile att

--- Stage0901/RocketNotes/src/pages/Profile/index.jsx
+++ Stage0901/RocketNotes/src/pages/Profile/index.jsx
@@ -1,5 +1,14 @@
 import { Container } from './styles'
+import { FiArrowLeft } from 'react-icons/fi'
 
 export function Profile() {
-  return <Container></Container>
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft />
+        </a>
+      </header>
+    </Container>
+  )
 }
