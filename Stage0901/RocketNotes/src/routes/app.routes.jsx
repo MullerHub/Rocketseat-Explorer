f365824c64a9f934220382154cc6e0dbdf7a2644
murilo muller <murilomuller@protonmail.com>
2022-09-28 00:04:16 -0300

    Routes Att

--- Stage0901/RocketNotes/src/routes/app.routes.jsx
+++ Stage0901/RocketNotes/src/routes/app.routes.jsx
@@ -8,7 +8,10 @@ import { Profile } from '../pages/Profile'
 export function AppRoutes() {
   return (
     <Routes>
-
+      <Route path='/' element={<Home />} />
+      <Route path='/new' element={<New />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/details/:id' element={<Details />} />
     </Routes>
   )
 }
