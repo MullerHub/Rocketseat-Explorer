37664e907b01e00f653f27bd1dd83c6df4a42f4b
murilo muller <murilomuller@protonmail.com>
2022-09-27 21:34:04 -0300

    Profile att

--- Stage0901/RocketNotes/src/pages/Profile/index.jsx
+++ Stage0901/RocketNotes/src/pages/Profile/index.jsx
@@ -2,6 +2,7 @@ import { Container } from './styles'
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
 
 import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
 import { Form } from 'react-bootstrap'
 
 export function Profile() {
