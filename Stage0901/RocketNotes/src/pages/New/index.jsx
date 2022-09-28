b838340a0bdf2077dd10cc58164556d9b2aae2bf
murilo muller <murilomuller@protonmail.com>
2022-09-27 22:24:26 -0300

    New att

--- Stage0901/RocketNotes/src/pages/New/index.jsx
+++ Stage0901/RocketNotes/src/pages/New/index.jsx
@@ -1,8 +1,11 @@
 import { Container } from './styles'
 
+import { Header } from '../../components/Header'
+
 export function New() {
   return (
     <Container>
+      <Header></Header>
       <div>ana Konda</div>
     </Container>
   )
