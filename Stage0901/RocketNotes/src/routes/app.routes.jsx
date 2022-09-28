cd3ce868728d7288191431ff483dbcbb308348d2
murilo muller <murilomuller@protonmail.com>
2022-09-28 00:02:30 -0300

    Routes Att

--- Stage0901/RocketNotes/src/routes/app.routes.jsx
+++ Stage0901/RocketNotes/src/routes/app.routes.jsx
@@ -1,1 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
+
+import { New } from '../pages/New'
+import { Home } from '../pages/Home'
+import { Details } from '../pages/Details'
+import { Profile } from '../pages/Profile'
+
+export function AppRoutes() {
+  return (
+    <Routes>
+
+    </Routes>
+  )
+}
