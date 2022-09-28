455ecf667bd5bef031e323b865cd0d5c969f117d
murilo muller <murilomuller@protonmail.com>
2022-09-27 22:58:48 -0300

    Textarea att

--- Stage0901/RocketNotes/src/components/Textarea/styles.js
+++ Stage0901/RocketNotes/src/components/Textarea/styles.js
@@ -4,8 +4,14 @@ export const Container = styled.textarea`
   width: 100%;
   height: 150px;
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
-  border: none;
-  padding: 10px;
-
   color: ${({ theme }) => theme.COLORS.WHITE};
+  border: none;
+  resize: none;
+
+  border-radius: 10px;
+  margin-bottom: 8px;
+  padding: 16px;
+
+  &::placeholder {
+  }
 `
