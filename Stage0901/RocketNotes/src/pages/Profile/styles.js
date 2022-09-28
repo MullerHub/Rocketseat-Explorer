7667122896645e1010acc0fc33613a54ba240694
murilo muller <murilomuller@protonmail.com>
2022-09-27 21:19:58 -0300

    Profile att

--- Stage0901/RocketNotes/src/pages/Profile/styles.js
+++ Stage0901/RocketNotes/src/pages/Profile/styles.js
@@ -2,4 +2,17 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   width: 100%;
+
+  > header {
+    color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+    display: flex;
+    align-items: center;
+
+    padding: 0 124px;
+
+    svg {
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+  }
 `
