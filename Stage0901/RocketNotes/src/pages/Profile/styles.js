1876a339b8a6067b837bfc9e1757196afbbe9f16
murilo muller <murilomuller@protonmail.com>
2022-09-27 21:26:42 -0300

    Profile att

--- Stage0901/RocketNotes/src/pages/Profile/styles.js
+++ Stage0901/RocketNotes/src/pages/Profile/styles.js
@@ -2,11 +2,12 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   width: 100%;
+  background-color: blue;
 
   > header {
     width: 100%;
     height: 144px;
-    color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
 
     display: flex;
     align-items: center;
