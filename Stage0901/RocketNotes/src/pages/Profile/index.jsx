178e5d1a4f9bc391bfb14aeb87cbb622b3e56a90
murilo muller <murilomuller@protonmail.com>
2022-09-27 21:33:35 -0300

    Profile att

--- Stage0901/RocketNotes/src/pages/Profile/index.jsx
+++ Stage0901/RocketNotes/src/pages/Profile/index.jsx
@@ -17,6 +17,7 @@ export function Profile() {
         <Input placeholder="Nome" type="text" icon={FiUser} />
         <Input placeholder="E-mail" type="text" icon={FiMail} />
         <Input placeholder="Senha atual" type="password" icon={FiLock} />
+        <Input placeholder="Nova senha" type="password" icon={FiLock} />
       </Form>
     </Container>
   )
