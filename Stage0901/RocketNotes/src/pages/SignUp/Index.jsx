8d4dbed3280b2318f3326c0f158949cbb5e137c0
murilo muller <murilomuller@protonmail.com>
2022-09-28 00:19:27 -0300

    Routes SignUp Att

--- Stage0901/RocketNotes/src/pages/SignUp/Index.jsx
+++ Stage0901/RocketNotes/src/pages/SignUp/Index.jsx
@@ -1,6 +1,7 @@
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 import { FiMail, FiLock, FiUser } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 import { Background, Container, Form } from './styles'
 
@@ -18,7 +19,7 @@ export function SignUp() {
         <Input placeholder="Senha" type="password" icon={FiLock} />
         <Button title="Cadastrar" />
 
-        <a href="#">Voltar para o login</a>
+        <Link to="/" >Voltar para o login</Link>
       </Form>
     </Container>
   )
