e1e5980e59458519082cf4f075175e524ba9f684
murilo muller <murilomuller@protonmail.com>
2022-09-28 00:35:01 -0300

    Routes Profile Att

--- Stage0901/RocketNotes/src/components/Header/index.jsx
+++ Stage0901/RocketNotes/src/components/Header/index.jsx
@@ -1,18 +1,18 @@
-import {RiShutDownLine} from 'react-icons/ri'
-import {Container, Profile, Logout } from './styles'
+import { RiShutDownLine } from 'react-icons/ri'
+import { Container, Profile, Logout } from './styles'
 
 export function Header() {
 
-  return(
+  return (
     <Container>
-      <Profile>
-      <img src='https://github.com/mullerhub.png'
-      alt='foto do usuario'
-       />
-       <div>
-        <span>Bem vindo</span>
-        <strong>Murilo Muller</strong>
-       </div>
+      <Profile to="/profile">
+        <img src='https://github.com/mullerhub.png'
+          alt='foto do usuario'
+        />
+        <div>
+          <span>Bem vindo</span>
+          <strong>Murilo Muller</strong>
+        </div>
       </Profile>
       <Logout>
         <RiShutDownLine />
--- Stage0901/RocketNotes/src/components/Header/styles.js
+++ Stage0901/RocketNotes/src/components/Header/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import theme from '../../styles/theme'
+import { Link } from 'react-router-dom'
 
 export const Container = styled.header`
   grid-area: header;
@@ -15,7 +16,7 @@ export const Container = styled.header`
   padding: 0 80px;
 `
 
-export const Profile = styled.div`
+export const Profile = styled(Link)`
   display: flex;
   align-items: center;
 
