b7559e54e4060eec9d76a9be5c431aa6207f0aa3
murilo muller <murilomuller@protonmail.com>
2022-09-27 22:52:42 -0300

    Textarea att

--- Stage0901/RocketNotes/src/pages/New/index.jsx
+++ Stage0901/RocketNotes/src/pages/New/index.jsx
@@ -2,6 +2,7 @@ import { Container, Form } from './styles'
 
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
+import { Textarea } from '../../components/Textarea'
 
 export function New() {
   return (
@@ -15,6 +16,7 @@ export function New() {
             <a href="/">Voltar</a>
           </header>
           <Input placeholder="Titulo" />
+          <Textarea placeholder="Observações" />
         </Form>
       </main>
     </Container>
