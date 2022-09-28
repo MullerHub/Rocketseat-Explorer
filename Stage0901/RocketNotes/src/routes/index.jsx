ad455ffc8d351dba149aa1107173b908ed3b61b9
murilo muller <murilomuller@protonmail.com>
2022-09-28 00:08:26 -0300

    Routes Att

--- Stage0901/RocketNotes/src/routes/index.jsx
+++ Stage0901/RocketNotes/src/routes/index.jsx
@@ -2,3 +2,10 @@ import { BrowserRouter } from "react-router-dom";
 
 import { AppRoutes } from "./app.routes";
 
+export function Routes() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
+}
