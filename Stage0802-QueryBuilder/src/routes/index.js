7f04c0f6c1149ea7aa9073009eee93429fdf216a
murilo muller <murilomuller@protonmail.com>
2022-09-29 11:43:22 -0300

    ajustes

--- Stage0802-QueryBuilder/src/routes/index.js
+++ Stage0802-QueryBuilder/src/routes/index.js
@@ -1,14 +1,13 @@
-const { Router } = require("express")
+const { Router } = require('express')
 
-const userRouter = require("./users.routes")
-const notesRouter = require("./notes.routes")
-const tagsRouter = require("./tags.routes ")
+const userRouter = require('./users.routes')
+const notesRouter = require('./notes.routes')
+const tagsRouter = require('./tags.routes ')
 
+const routes = Router()
 
-const routes = Router();
+routes.use('/users', userRouter)
+routes.use('/notes', notesRouter)
+routes.use('/tags', tagsRouter)
 
-routes.use("/users", userRouter)
-routes.use("/notes", notesRouter)
-routes.use("/tags", tagsRouter)
-
-module.exports = routes;
+module.exports = routes
