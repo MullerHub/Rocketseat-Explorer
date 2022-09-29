63a6a1de39eb135bd1de5b3acd7334f861092cb0
murilo muller <murilomuller@protonmail.com>
2022-09-29 19:58:11 -0300

    Home att

--- Stage0902_desafio/RocketMovies/src/main.jsx
+++ Stage0902_desafio/RocketMovies/src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import GlobalStyles from './styles/global'
+import theme from './styles/theme'
+
+import { ThemeProvider } from 'styled-components'
+import { Home } from './pages/Home/index'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles>
+        <Home />
+      </GlobalStyles>
+    </ThemeProvider>
   </React.StrictMode>
 )
--- Stage0902_desafio/RocketMovies/src/pages/Home/index.jsx
+++ Stage0902_desafio/RocketMovies/src/pages/Home/index.jsx
@@ -1,7 +1,9 @@
 import { Container } from './styles'
 
 export function Home() {
-  <Container>
-    <div>kimm</div>
-  </Container>
+  return (
+    <Container>
+      <div>kimm</div>
+    </Container>
+  )
 }
