d22c766282e5f34e9deafee1d282af5b7802967e
murilo muller <murilomuller@protonmail.com>
2022-09-29 19:38:00 -0300

    RocketMovies png add

---
+++ Stage0902_desafio/RocketMovies/src/styles/theme.js
@@ -0,0 +1,17 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: '#1C1B1E',
+    BACKGROUND_800: '#3E3B47',
+    BACKGROUND_700: '#948F99',
+
+    WHITE: '#F4EDE8',
+    BUTTON: '#FF859B',
+    BUTTON_BG: 'RGBA(255,133,155,0.4)',
+    BUTTON_BG_2: '#282124',
+    BUTTON_BG_3: '#312E38',
+
+    GRAY_300: '#666360',
+
+    RED: '#FF002E'
+  }
+}
