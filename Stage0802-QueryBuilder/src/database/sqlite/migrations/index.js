980b5ec0737ed52b85faed9cc632c577b845cdb1
murilo muller <murilomuller@protonmail.com>
2022-09-29 11:47:33 -0300

    ajustes

--- Stage0802-QueryBuilder/src/database/sqlite/migrations/index.js
+++ Stage0802-QueryBuilder/src/database/sqlite/migrations/index.js
@@ -1,14 +1,15 @@
-const sqliteConnection = require('../../sqlite');
-const createUsers = require('./createUsers');
+const sqliteConnection = require('../../sqlite')
+const createUsers = require('./createUsers')
 
-async function migrationsRun(){
-  const schemas = [ /* schemas se refere as tabelas do BdeD */
-  createUsers
-  ].join('');
+async function migrationsRun() {
+  const schemas = [
+    /* schemas se refere as tabelas do BdeD */
+    createUsers
+  ].join('')
 
   sqliteConnection()
-  .then(db => db.exec(schemas))
-  .catch(error => console.error(error));
+    .then(db => db.exec(schemas))
+    .catch(error => console.error(error))
 }
 
-module.exports = migrationsRun;
+module.exports = migrationsRun
