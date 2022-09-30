21e447879ce130a1ec7e60278e683d101e188c4f
murilo muller <murilomuller@protonmail.com>
2022-09-30 00:06:40 -0300

    SignUp  att

--- Stage0902_desafio/RocketMovies/src/pages/SignUp/Index.jsx
+++ Stage0902_desafio/RocketMovies/src/pages/SignUp/Index.jsx
@@ -12,9 +12,9 @@ export function SignUp() {
         <p>Aplicação para salvar e gerenciar seus links úteis</p>
         <h2>Crie sua conta</h2>
 
-        <Input placeholder="Nome" type="text" icon={FiUser} />
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input placeholder="Nome" type="text" />
+        <Input placeholder="E-mail" type="text" />
+        <Input placeholder="Senha" type="password" />
         <Button title="Cadastrar" />
 
         <Link to="/" >Voltar para o login</Link>
