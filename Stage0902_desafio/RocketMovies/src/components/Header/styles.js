ede57458459612f0459a397049b058bac8e12a40
murilo muller <murilomuller@protonmail.com>
2022-09-30 00:17:52 -0300

    Header styles add

---
+++ Stage0902_desafio/RocketMovies/src/components/Header/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  height: 116px;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+`
