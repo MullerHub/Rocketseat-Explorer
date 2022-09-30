62e68de8badcf519a053752db98b72c7bd777161
murilo muller <murilomuller@protonmail.com>
2022-09-30 00:20:34 -0300

    Header add

--- Stage0902_desafio/RocketMovies/src/components/Header/index.jsx
+++ Stage0902_desafio/RocketMovies/src/components/Header/index.jsx
@@ -1,1 +1,12 @@
 import { Container } from './styles'
+
+export function Header() {
+  <Container>
+    <h1>RocketMovies</h1>
+    <input />
+    <strong>Murilo Muller</strong>
+    <span>Sair</span>
+    <a > aaa<img src='https://www.mullerhub.com' /></a>
+
+  </Container>
+}
