3a51a6ed5cb31e279e172f7202652c35291bf0b2
murilo muller <murilomuller@protonmail.com>
2022-09-30 00:05:05 -0300

    SignIn  att

---
+++ Stage0902_desafio/RocketMovies/src/pages/SignIn/Index.jsx
@@ -0,0 +1,23 @@
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { Background, Container, Form } from './styles'
+
+export function SignIn() {
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis</p>
+        <h2>Faça seu login</h2>
+
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Button title="Entrar" />
+
+        <Link to="/register">Criar conta</Link>
+      </Form>
+      <Background />
+    </Container>
+  )
+}
