55e4b6898b1fcc9e1b8d22d7b985d61ff49a7973
murilo muller <murilomuller@protonmail.com>
2022-09-30 00:07:15 -0300

    SignIn  att

--- Stage0902_desafio/RocketMovies/src/pages/SignIn/Index.jsx
+++ Stage0902_desafio/RocketMovies/src/pages/SignIn/Index.jsx
@@ -7,7 +7,7 @@ export function SignIn() {
   return (
     <Container>
       <Form>
-        <h1>Rocket Notes</h1>
+        <h1>Rocket Movies</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis</p>
         <h2>Faça seu login</h2>
 
