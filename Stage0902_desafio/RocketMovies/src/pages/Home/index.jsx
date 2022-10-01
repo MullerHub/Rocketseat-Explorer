b4811efce2b1b85d52d147ca087284f205316da5
murilo muller <murilomuller@protonmail.com>
2022-10-01 00:34:42 -0300

    Home att

--- Stage0902_desafio/RocketMovies/src/pages/Home/index.jsx
+++ Stage0902_desafio/RocketMovies/src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { Container, Teste } from './styles'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
+import { FiMail } from 'react-icons/fi'
 
 export function Home() {
   return (
@@ -8,7 +9,7 @@ export function Home() {
       <Header />
       <div>
         <h1>Meus filmes</h1>
-        <Button />
+        <Button icon={FiMail} title="Adicionar filme" />
       </div>
 
     </Container>
--- Stage0902_desafio/RocketMovies/src/pages/Home/styles.js
+++ Stage0902_desafio/RocketMovies/src/pages/Home/styles.js
@@ -4,9 +4,19 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: ${({ theme }) => theme.COLORS.BUTTON_BG};
-  position: absolute;
-  z-index: 1;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+  > div {
+    border-top: 2px solid blue;
+    display: flex;
+    padding: 10px 90px;
+    justify-content: space-between;
+
+    button {
+      max-width: 100px;
+    }
+  }
 `
 
 export const Teste = styled.section`
