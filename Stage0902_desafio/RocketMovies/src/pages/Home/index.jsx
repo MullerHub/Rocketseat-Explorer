4bba93a3f639785612a097f3b8c0b5d7e5d5fe97
murilo muller <murilomuller@protonmail.com>
2022-10-01 00:43:46 -0300

    SectionMovies styles add

--- Stage0902_desafio/RocketMovies/src/pages/Home/index.jsx
+++ Stage0902_desafio/RocketMovies/src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import { Container, Teste } from './styles'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { FiMail } from 'react-icons/fi'
+import { SectionMovies } from '../../components/SectionMovies'
 
 export function Home() {
   return (
@@ -12,6 +13,7 @@ export function Home() {
         <Button icon={FiMail} title="Adicionar filme" />
       </div>
 
+      <SectionMovies />
 
 
     </Container>
