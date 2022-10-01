afbd2609b5337623e4b0be59d7a1ae36cd60c240
murilo muller <murilomuller@protonmail.com>
2022-10-01 00:21:49 -0300

    Profile att

--- Stage0902_desafio/RocketMovies/src/pages/Profile/styles.js
+++ Stage0902_desafio/RocketMovies/src/pages/Profile/styles.js
@@ -20,12 +20,58 @@ export const Container = styled.div`
 
 export const Form = styled.form`
   max-width: 100%;
+
+  margin: 50px 0 30px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  > div:nth-child(4) {
+    background-color: blue;
+    margin-top: 20px;
+  }
+
+  > button {
+    margin-top: 20px;
+  }
 `
 
 export const Avatar = styled.div`
   width: 100%;
+  position: relative;
+  margin: -75px auto 32px;
+  width: 150px;
+  height: 150px;
+
+  > img {
+    border-radius: 50%;
+    width: 150px;
+    height: 150px;
+  }
+
+  > label {
+    width: 50px;
+    height: 50px;
+    background-color: ${({ theme }) => theme.COLORS.BUTTON};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    cursor: pointer;
+  }
+
+  input {
+    display: none;
+  }
+
+  svg {
+    width: 20px;
+    height: 20px;
+    color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+  }
 `
