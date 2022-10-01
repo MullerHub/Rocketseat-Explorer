a83236ffdfd2d25a6a6ad32121cd5fb42d757dd7
murilo muller <murilomuller@protonmail.com>
2022-10-01 12:48:54 -0300

    SectionMovies att

--- Stage0902_desafio/RocketMovies/src/main.jsx
+++ Stage0902_desafio/RocketMovies/src/main.jsx
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom/client'
 import theme from './styles/theme'
 import React from 'react'
 
-import { Home } from './pages/Home'
+import { CreateMovie } from './pages/CreateMovie'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <CreateMovie />
     </ThemeProvider>
   </React.StrictMode>
 )
