90091f3fa9fd110f22ea8d5d3bdddc9dd0b21433
murilo muller <murilomuller@protonmail.com>
2022-09-30 22:57:43 -0300

    Profile att

--- Stage0902_desafio/RocketMovies/src/main.jsx
+++ Stage0902_desafio/RocketMovies/src/main.jsx
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom/client'
 import theme from './styles/theme'
 import React from 'react'
 
-import { SignUp } from './pages/SignUp/Index'
+import { Profile } from './pages/Profile'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 )
