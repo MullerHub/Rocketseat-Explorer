a07aa7e41f7cd576d2aa927ca660e2e41de80a2b
murilo muller <murilomuller@protonmail.com>
2022-10-01 15:42:08 -0300

    textarea att

--- Stage0902_desafio/RocketMovies/src/pages/CreateMovie/index.jsx
+++ Stage0902_desafio/RocketMovies/src/pages/CreateMovie/index.jsx
@@ -1,38 +1,43 @@
-import { Container, Content, Marcadores } from './styles'
+import { Container, Content, Marcadores, Form } from './styles'
+import { AiOutlinePlus, AiOutlineClose } from 'react-icons/ai'
+
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
-import { AiOutlinePlus, AiOutlineClose } from 'react-icons/ai'
 import { Textarea } from '../../components/TextArea'
 
 export function CreateMovie() {
   return (
     <Container>
-      <Header />
-      <i> Voltar</i>
+      <Form>
 
-      <Content>
-        <div>
-          <Input placeholder="teste" type="text" />
-          <Input placeholder="teste 123" type="text" />
-        </div>
-        <Textarea placeholder="Testando" />
-      </Content>
+        <Header />
+        <i> Voltar</i>
 
-      <Marcadores>
-        <h2>Marcadores</h2>
+        <Content>
+          <div>
+            <Input placeholder="teste" type="text" />
+            <Input placeholder="teste 123" type="text" />
+          </div>
+          <Textarea placeholder="Testando" />
+        </Content>
 
-        <div>
-          <Button title="React" icon={AiOutlineClose} />
-          <Button title="Novo marcador" icon={AiOutlinePlus} />
-        </div>
-      </Marcadores>
+        <Marcadores>
+          <h2>Marcadores</h2>
+
+          <div>
+            <Button title="React" icon={AiOutlineClose} />
+            <Button title="Novo marcador" icon={AiOutlinePlus} />
+          </div>
+        </Marcadores>
 
 
-      <footer>
-        <Button title="Exccluir filme" />
-        <Button title="Salvar alterações" />
-      </footer>
+        <footer>
+          <Button title="Exccluir filme" />
+          <Button title="Salvar alterações" />
+        </footer>
+      </Form >
     </Container>
+
   )
 }
