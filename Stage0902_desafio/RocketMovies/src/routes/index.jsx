be882f119c9655f3b5829841369b2002acc2fde4
murilo muller <murilomuller@protonmail.com>
2022-10-01 20:57:53 -0300

    Routes atualizado

---
+++ Stage0902_desafio/RocketMovies/src/routes/index.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { AppRoutes } from "./app.routes";
+
+export function Routes() {
+  <BrowserRouter>
+    <AppRoutes />
+  </BrowserRouter>
+}
