4b91f05f6ca9e0a82fb75746df567eab957b6a37
murilo muller <murilomuller@protonmail.com>
2022-10-02 16:27:12 -0300

    Routes Sessions index att

--- Stage0802-QueryBuilder/src/routes/index.js
+++ Stage0802-QueryBuilder/src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require('express')
 const userRouter = require('./users.routes')
 const notesRouter = require('./notes.routes')
 const tagsRouter = require('./tags.routes ')
+const sessionsRoutes = require('./sessions.routes')
 
 const routes = Router()
 
 routes.use('/users', userRouter)
 routes.use('/notes', notesRouter)
 routes.use('/tags', tagsRouter)
+routes.use('/sessions', sessionsRoutes)
 
 module.exports = routes
