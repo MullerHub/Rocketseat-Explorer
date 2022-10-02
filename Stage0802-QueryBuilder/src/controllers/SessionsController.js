d74b61fd19f8597c34ee74d7ced8e3e25f13b291
murilo muller <murilomuller@protonmail.com>
2022-10-02 17:40:30 -0300

    auth Session att

--- Stage0802-QueryBuilder/src/controllers/SessionsController.js
+++ Stage0802-QueryBuilder/src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const knex = require('../database/knex')
 const AppError = require('../utils/AppError')
 const { compare } = require('bcryptjs')
+const authConfig = require('../configs/auth')
+const { sign } = require('jsonwebtoken')
 
 class SessionController {
   async create(request, response) {
@@ -17,6 +19,8 @@ class SessionController {
     if (!passwordMatched) {
       throw new AppError('E-mail e/ou senha incorreta', 401)
     }
+
+    const { secret, expiresIn } = authConfig.jwt
 
     return response.json(user)
   }
