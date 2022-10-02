4c9c00c2726927fdb7e0b739ae4ac15d7c162671
murilo muller <murilomuller@protonmail.com>
2022-10-02 19:17:12 -0300

    middlewares authenticate att

--- Stage0802-QueryBuilder/src/middlewares/ensureAuthenticated.js
+++ Stage0802-QueryBuilder/src/middlewares/ensureAuthenticated.js
@@ -1,3 +1,21 @@
 const { verify } = require('jsonwebtoken')
 const AppError = require('../utils/AppError')
 const authConfig = require('../configs/auth')
+
+function ensureAuthenticated(request, response, next) {
+  const authHeader = request.headers.authorization
+
+  if (!authHeader) {
+    throw new AppError('JWT Token inválido', 401)
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const { sub: user_id } = verify(token, authConfig.jwt.secret)
+
+    request.user = {
+      id: Number(user_id)
+    }
+  } catch {}
+}
