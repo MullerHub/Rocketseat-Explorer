646b0b6280658988da06b24547e67b944459e869
murilo muller <murilomuller@protonmail.com>
2022-10-02 16:40:30 -0300

    Routes Sessions att

--- Stage0802-QueryBuilder/src/controllers/SessionsController.js
+++ Stage0802-QueryBuilder/src/controllers/SessionsController.js
@@ -4,7 +4,10 @@ const AppError = require('../utils/AppError')
 class SessionController {
   async create(request, response) {
     const { email, password } = request.body
-    return response.json({ email, password })
+
+    const user = await knex('users').where({ email }).first()
+
+    return response.json(user)
   }
 }
 
