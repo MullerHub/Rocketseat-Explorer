336d40895fd89b895dbfdbaa0778eb592779b0c0
murilo muller <murilomuller@protonmail.com>
2022-10-02 20:14:40 -0300

    middlewares authenticate att

--- Stage0802-QueryBuilder/src/routes/notes.routes.js
+++ Stage0802-QueryBuilder/src/routes/notes.routes.js
@@ -1,14 +1,16 @@
 const { Router } = require('express')
 
 const NotesController = require('../controllers/NotesController')
-
+const notesController = new NotesController()
 const notesRoutes = Router()
 
-const notesController = new NotesController()
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
-notesRoutes.get('/', notesController.index)
-notesRoutes.post('/:user_id', notesController.create)
+notesRoutes.use(ensureAuthenticated)
+
+notesRoutes.post('/', notesController.create)
 notesRoutes.get('/:id', notesController.show)
 notesRoutes.delete('/:id', notesController.delete)
+notesRoutes.get('/', notesController.index)
 
 module.exports = notesRoutes
