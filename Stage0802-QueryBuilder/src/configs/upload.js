c2515e62690fa831914f1c550b17ea0e1f6c0eb0
murilo muller <murilomuller@protonmail.com>
2022-10-02 20:42:02 -0300

    upload att

--- Stage0802-QueryBuilder/src/configs/upload.js
+++ Stage0802-QueryBuilder/src/configs/upload.js
@@ -1,3 +1,12 @@
 const path = require('path')
+const multer = require('multer')
+const crypto = require('crypto')
 
-const TPM_FOLDER = path.resolve(__dirname, '..', '..', 'tmp')
+const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp')
+const UPLOADS_FOLDER = PATH.RESOLVE(__dirname, 'uploads')
+
+const MULTER = {
+  storage: multer.diskStorage({
+    destination: TMP_FOLDER
+  })
+}
