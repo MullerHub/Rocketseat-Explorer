115d2683692b354e5b823cf438dbb96c89c2a808
murilo muller <murilomuller@protonmail.com>
2022-10-02 16:31:44 -0300

    Routes Sessions att

--- Stage0802-QueryBuilder/src/controllers/SessionsController.js
+++ Stage0802-QueryBuilder/src/controllers/SessionsController.js
@@ -1,5 +1,8 @@
 class SessionController {
-  async create() {}
+  async create(request, response) {
+    const { email, password } = request.body
+    return response.json({ email, password })
+  }
 }
 
 module.exports = SessionsController
