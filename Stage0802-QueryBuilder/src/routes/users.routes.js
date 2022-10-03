4d4ed213b55a9753da325d50db0a300591ff40f5
murilo muller <murilomuller@protonmail.com>
2022-10-03 10:24:54 -0300

    User Avatar att

--- Stage0802-QueryBuilder/src/routes/users.routes.js
+++ Stage0802-QueryBuilder/src/routes/users.routes.js
@@ -1,14 +1,16 @@
 const { Router } = require('express')
-
 const multer = require('multer')
 const uploadConfig = require('../configs/upload')
 
 const UsersController = require('../controllers/UsersController')
+const UserAvatarController = require('../controllers/UserAvatarController')
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const userRoutes = Router()
 const upload = multer(uploadConfig.MULTER)
+
 const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
 userRoutes.post('/', usersController.create)
 userRoutes.put('/', ensureAuthenticated, usersController.update)
@@ -16,10 +18,7 @@ userRoutes.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  (request, response) => {
-    console.log(request.file.filename)
-    response.json()
-  }
+  userAvatarController.update
 )
 
 module.exports = userRoutes
