9cb853ce960dd7c94059fe03658c6877ec426f8c
murilo muller <murilomuller@protonmail.com>
2022-10-03 09:28:12 -0300

    Routes upload img add

--- Stage0802-QueryBuilder/src/routes/users.routes.js
+++ Stage0802-QueryBuilder/src/routes/users.routes.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const { Router } = require('express')
 
 const UsersController = require('../controllers/UsersController')
@@ -5,16 +6,22 @@ const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const userRoutes = Router()
 
-/*
-function myMidlewarre(request, response, next) {
-  console.log("iniciou o server");
-
-  next();
-}  */
+/*   function myMidlewarre(request, response, next) {
+        console.log("iniciou o server");
+        next(); }  */
 
 const usersController = new UsersController()
 
 userRoutes.post('/', usersController.create)
 userRoutes.put('/', ensureAuthenticated, usersController.update)
+userRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  (req, res) => {
+    console.log(request.file.filename)
+    response.json()
+  }
+)
 
 module.exports = userRoutes
