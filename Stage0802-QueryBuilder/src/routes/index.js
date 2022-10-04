f03ffcfe5a0348de9ca16af8fc06e9cdae2d5b28
murilo muller <murilomuller@protonmail.com>
2022-10-04 01:00:57 -0300

    index routes adjusts

--- Stage0802-QueryBuilder/src/routes/index.js
+++ Stage0802-QueryBuilder/src/routes/index.js
@@ -1,13 +1,13 @@
 const { Router } = require('express')
 
-const userRouter = require('./users.routes')
+const usersRouter = require('./users.routes')
 const notesRouter = require('./notes.routes')
 const tagsRouter = require('./tags.routes')
 const sessionsRoutes = require('./sessions.routes')
 
 const routes = Router()
 
-routes.use('/users', userRouter)
+routes.use('/users', usersRouter)
 routes.use('/notes', notesRouter)
 routes.use('/tags', tagsRouter)
 routes.use('/sessions', sessionsRoutes)
