373063ef33cc986ee71236ea79973dc754f4d368
Murilo_Muller <murilomuller@protonmail.com>
2022-10-06 18:37:02 -0300

    hooks auth add

--- Stage0901/RocketNotes/src/hooks/auth.js
+++ Stage0901/RocketNotes/src/hooks/auth.js
@@ -3,5 +3,9 @@ import { createContext } from 'react'
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
-  return <AuthContext.Provider>{children}</AuthContext.Provider>
+  return (
+    <AuthContext.Provider value={{ name: 'ana', email: 'ana@ana.com' }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
