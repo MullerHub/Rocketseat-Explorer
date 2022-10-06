a9b942c234ba95c03f213f27537c298aac6304ad
Murilo_Muller <murilomuller@protonmail.com>
2022-10-06 18:35:57 -0300

    hooks auth add

--- Stage0901/RocketNotes/src/hooks/auth.js
+++ Stage0901/RocketNotes/src/hooks/auth.js
@@ -2,9 +2,6 @@ import { createContext } from 'react'
 
 export const AuthContext = createContext({})
 
-
-function AuthProvider({children}) {
-  return(
-    
-  )
+function AuthProvider({ children }) {
+  return <AuthContext.Provider>{children}</AuthContext.Provider>
 }
