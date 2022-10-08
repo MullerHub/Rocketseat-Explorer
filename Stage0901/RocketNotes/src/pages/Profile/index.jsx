0edf69409f1f16e306e8abc313a884ec5807b73d
Murilo_Muller <murilomuller@protonmail.com>
2022-10-08 00:01:53 -0300

    Profile update

--- Stage0901/RocketNotes/src/pages/Profile/index.jsx
+++ Stage0901/RocketNotes/src/pages/Profile/index.jsx
@@ -35,10 +35,34 @@ export function Profile() {
             <input id="avatar" type="file" />
           </label>
         </Avatar>
-        <Input placeholder="Nome" type="text" icon={FiUser} />
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha atual" type="password" icon={FiLock} />
-        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+
+        />
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+
+        />
+
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+
+        />
+
+        <Input
+          placeholder="Nova senha"
+          type="password"
+          icon={FiLock}
+
+        />
+
 
         <Button title="Salvar" />
       </Form>
