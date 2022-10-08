c1bf9d05f40344ba22b155b7b3e224068cdfcaf0
Murilo_Muller <murilomuller@protonmail.com>
2022-10-08 00:05:33 -0300

    Profile update

--- Stage0901/RocketNotes/src/pages/Profile/index.jsx
+++ Stage0901/RocketNotes/src/pages/Profile/index.jsx
@@ -56,6 +56,7 @@ export function Profile() {
           placeholder="Senha atual"
           type="password"
           icon={FiLock}
+          onChange={e => setPasswordOld(e.target.value)}
 
         />
 
