14191519ce584afb74147e2fbad9e9f01acef6b1
Murilo_Muller <murilomuller@protonmail.com>
2022-10-08 12:10:07 -0300

    update profile

--- Stage0901/RocketNotes/src/pages/Profile/index.jsx
+++ Stage0901/RocketNotes/src/pages/Profile/index.jsx
@@ -9,11 +9,21 @@ import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
 export function Profile() {
-  const { user } = useAuth()
+  const { user, updateProfile } = useAuth()
   const [name, setName] = useState(user.name)
   const [email, setEmail] = useState(user.email)
   const [passwordOld, setPasswordOld] = useState()
   const [passwordNew, setPasswordNew] = useState()
+
+  async function handleUpdate() {
+    const user = {
+      name,
+      email,
+      password: passwordNew,
+      old_password: passwordOld,
+    }
+    await updateProfile({})
+  }
 
   return (
     <Container>
