78f218128f20eace1ed2cf45babe27a0a7417acb
Murilo_Muller <murilomuller@protonmail.com>
2022-10-13 15:44:47 -0300

    ajustes no knex createTable

--- Stage0802-QueryBuilder/src/database/knex/migrations/20220719024939_createTags.js
+++ Stage0802-QueryBuilder/src/database/knex/migrations/20220719024939_createTags.js
@@ -1,13 +1,15 @@
-exports.up = knex => knex.schema.createTable("tags", table => {
-  table.increments("id")
-  table.text("name").notNullable()
+exports.up = knex =>
+  knex.schema.createTable('tags', table => {
+    table.increments('id')
+    table.text('name').notNullable()
+    table
+      .integer('note_id')
+      .references('id')
+      .inTable('notes')
+      .onDelete('CASCADE')
+    table.integer('user_id').references('id').inTable('users')
+  })
 
-  table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE"); 
-  table.integer("user_id").references("id").inTable("users");
-  
-}); 
-
-exports.down = knex => knex.schema.dropTable("notes");
-
+exports.down = knex => knex.schema.dropTable('notes')
 
 /* CASCADE Deleta as tags vinculadas a nota, em cascata */
